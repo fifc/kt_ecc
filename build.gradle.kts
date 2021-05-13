45916bb81b94469b9e2293bf1da248f6391b7c42
Steven I <you@example.com>
2021-05-13 20:35:13 +0800

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val gradle_version = "7.0"
+val gradle_version = "7.0.1"
 
 buildscript {
 	extra.apply {
-		set("kotlinVersion", "1.5.0-RC")
+		set("kotlinVersion", "1.5.0")
 	}
 	repositories {
 		mavenCentral()
